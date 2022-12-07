1c2766883e05747c1733b0b20aff7d8a1c74dd17
AlfonsusOktarioSutomo <75030974+AlfonsusOktarioSutomo@users.noreply.github.com>
2022-12-07 22:24:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <style>
+            table{
+                width: 100%;
+            }
+            th, td, tr{
+                padding: 15px;
+            }
+
+        </style>
     </head>
     <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -24,87 +33,180 @@
         </div>  
         </nav>
         <br>
-        <div class="dropdown">
-        <select type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="color:white;">
-            <div class="dropdown-menu">
-                <?php
-                $csv = fopen("biaya.csv", "r");
-                $start_row = 0; //define start row
-                $i = 0; //define row count flag
-                $arr = array();
-                while (($row = fgetcsv($csv)) !== FALSE) {
-                    $arr[] = $row;
-                }
-                fclose($csv);
-                while($i < sizeof($arr)){
-                    echo  '<option value = "',$i,'"class="dropdown-item" style="color:black; background-color:white; border; 1px black">',  $arr[$i][0], '</option>';
-                    $i++;
-                }
-                ?>
-
-            </div>    
-        </select>
-        </div>
-
-        <div class="container">
-            <?php
-            if(file_exists('prasyarat.json')) {
-                $filename = 'prasyarat.json';
-                $data = file_get_contents($filename); //data read from json file
-                // print_r($data);
-                $users = json_decode($data);  //decode a data
-            
-                // print_r($users); //array format data printing
-                 $message = "<h3 class='text-success'>JSON file data</h3>";
-            }
-            else {
-                 $message = "<h3 class='text-danger'>JSON file Not found</h3>";
-            }
-            // // Read the JSON file 
-            // $json = file_get_contents('prasyarat.json');
-                
-            // // Decode the JSON file
-            // $json_data = json_decode($json,true);
-                
-            // // Display data
-            // print_r($json);
-            ?>
-            <div class="table-container">
-                <?php
-                        if(isset($message))
-                        {
-                            echo $message;
-
-                        ?>
-                    <table id="tbstyle">
-                        <tbody>
-                            <tr>
-                                <th>Kode</th>
-                                <th>Nama</th>
-                                <th>SKS</th>
-                                <th>WAAJIB</th>
-                                <th>Semester</th>
-                            </tr>
-                            <?php foreach ($users as $user) { ?>
-                            <tr>
-                                <td> <?= $user->kode; ?> </td>
-                                <td> <?= $user->nama; ?> </td>
-                                <td> <?= $user->sks; ?> </td>
-                                <td> <?= $user->wajib; ?> </td>
-                                <td> <?= $user->semester; ?> </td>
-                            </tr>
-                            <?php }
+        <form method = "POST">
+            <div class="dropdown">
+                <select type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="color:white;">
+                    <div class="dropdown-menu">
+                        <?php
+                        $csv = fopen("biaya.csv", "r");
+                        $start_row = 0; //define start row
+                        $i = 0; //define row count flag
+                        $arr = array();
+                        while (($row = fgetcsv($csv)) !== FALSE) {
+                            $arr[] = $row;
                         }
-                        else
-                            echo $message;
+                        fclose($csv);
+                        while($i < sizeof($arr)){
+                            echo  '<option value = "',$i,'"class="dropdown-item" style="color:black; background-color:white; border; 1px black">',  $arr[$i][0], '</option>';
+                            $i++;
+                        }
+                        
                         ?>
-                </tbody>
-            </table>
-            </div>
-        </div>
-        
-        <div>
+                    </div>    
+                </select>
 
-        </div>
+                <div class="container">
+                    <?php
+                    if(file_exists('prasyarat.json')) {
+                        $filename = 'prasyarat.json';
+                        $data = file_get_contents($filename); //data read from json file
+                        // print_r($data);
+                        $users = json_decode($data);  //decode a data
+                    
+                        // print_r($users); //array format data printing
+                        $message = "<h3 class='text-success'>Daftar Mata Kuliah</h3>";
+                    }
+                    else {
+                        $message = "<h3 class='text-danger'>JSON file Not found</h3>";
+                    }
+                    // // Read the JSON file 
+                    // $json = file_get_contents('prasyarat.json');
+                        
+                    // // Decode the JSON file
+                    // $json_data = json_decode($json,true);
+                        
+                    // // Display data
+                    // print_r($json);
+                    ?>
+                    <div class="table-container">
+                        <?php
+                                if(isset($message))
+                                {
+                                    echo $message;
+
+                                ?>
+                            <h2> Semester 1</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 1){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 2</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 2){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 3</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 3){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 4</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 4){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 5</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 5){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 6</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 6){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 7</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 7){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}?>
+                                </tbody>
+                            </table>
+                            <h2> Semester 8</h2>
+                            <table id="tbstyle" >
+                                <tbody>
+                                    <?php foreach ($users as $user) { ?>
+                                    <tr>
+                                        <?php if($user->semester == 8){?>
+                                        <td> <?= $user->kode; ?> </td>
+                                        <td> <?= $user->nama; ?> </td>
+                                        <td> <?= $user->sks; ?> </td>
+                                        <td> <input type="checkbox" id="check" name="<?= $user->name?>" value= "<?= $user->nama;?>"> </td>
+                                    </tr>
+                                    <?php }}
+                                }
+                                else
+                                    echo $message;
+                                ?>
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+
+        
     </body>
 </html>
